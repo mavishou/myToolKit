3eb0ede2ea7756d9307aacad70a144c3beb12f46
Mei Hou <houm@mail.cbi.pku.edu.cn>
2015-10-11 16:13:42 +0800

    get_exons.py & get_trans.py

---
+++ bio/gtf/get_trans.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+'''
+Get trans lines of each gtf file and simplify the 9th col
+'''
+
+import sys
+from gtf_util import processAnnotation
+
+for line in sys.stdin.readlines():
+	line = line.rstrip('\n')
+	# exclude comment lines
+	if not line.startswith('#'):
+		line = line.split('\t')
+		if line[2] == 'transcript':
+			dAnno = processAnnotation(line[8])
+			line[8] = 'gene_id \"' + dAnno['gene_id'] + '\"; transcript_id \"' + dAnno['transcript_id'] + '\";'
+			outLine = '\t'.join(line)
+			print outLine
+	dAnno = []
