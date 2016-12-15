5a3ed20b1f61398f6eaacb7b3a251792edb46598
Mei Hou <houm@mail.cbi.pku.edu.cn>
2016-12-15 16:06:36 +0800

    style(gtf): indent

--- bio/gtf/gtf_util.py
+++ bio/gtf/gtf_util.py
@@ -6,17 +6,17 @@ This script contains some useful functions
 '''
 import sys
 
-def processGTFAnno(anno):
-	dAnnos = {}
-	annos = anno.split(';')
-	annos = [a.strip() for a in annos if a != '' and a != ' ']
-	for a in annos:
-		lA = a.split(' ')
-		if len(lA) > 2:
-			print 'The annotation is not in the right format: %s' % anno
-			print lA
-			sys.exit(1)
-		dAnnos[lA[0]] = lA[1].strip('"')
-	return(dAnnos)
 
-	
+def processGTFAnno(anno):
+    dAnnos = {}
+    annos = anno.split(';')
+    annos = [a.strip() for a in annos if a != '' and a != ' ']
+    for a in annos:
+        lA = a.split(' ')
+        if len(lA) > 2:
+            print 'The annotation is not in the right format: %s' % anno
+            print lA
+            sys.exit(1)
+        dAnnos[lA[0]] = lA[1].strip('"')
+    return (dAnnos)
+
